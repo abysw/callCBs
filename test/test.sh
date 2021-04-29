256843ff46a4d484e399c3df1eccbaebae4298b7
Yuan-SW-F <49896080+Yuan-SW-F@users.noreply.github.com>
2021-04-29 10:40:30 +0800

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,5 @@
-export PAYH=$PWD/..:$PATH
+chmod 755 ../lastz-axt.sh ../call_CBs.py ../multiz-tba.012109/bin -R
+export PATH=$PWD/../multiz-tba.012109/bin:$PATH
+export PATH=$PWD/..:$PATH
 gunzip MtrunA17MT_Metru.roast.maf.gz
 sh ../CallCBs.sh
